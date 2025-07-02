9b5af38661dfb7f59e76e2b998c5911436bae8fb
MOUNESH-M <mounesh96290@gmail.com>
2025-07-02 12:44:22 +0530

    Suspicious object alert system integrated

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ def start_detection():
     cap = cv2.VideoCapture(0)
     model = YOLO('yolov8n.pt') 
 
+    suspicious_objects=['backpack','knife','bottle','suitcase']
+
     if not cap.isOpened():
         print("Error: Cannot open camera")
         return
@@ -21,18 +23,22 @@ def start_detection():
         
         for result in results:
             boxes = result.boxes
+            names=result.names
             for box in boxes:
-                x1, y1, x2, y2 = map(int, box.xyxy[0])  
-                conf = box.conf[0]  
-                cls = int(box.cls[0])  
+                cls_id=int(box.cls[0])
+                label=names[cls_id]
 
-               
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(frame, f'{model.names[cls]} {conf:.2f}', (x1, y1 - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                x1,y1,x2,y2=map(int,box.xyxy[0])
+                cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
+                cv2.putText(frame,label,(x1,y1-10),cv2.FONT_HERSHEY_COMPLEX,0.9,(0,255,0),2)
 
+                if label in suspicious_objects:
+                    cv2.putText(
+                        frame,"ALERT: Suspicious object detected..!",(50,50),
+                        cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2
+                        )
        
-        cv2.imshow('YOLO Detection', frame)
+        cv2.imshow('Multi-Modal Surveillance', frame)
 
        
         if cv2.waitKey(1) & 0xFF == ord('q'):
