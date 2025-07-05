d46c0aedfc7d05d9be884a530d6a75c5bbcf91d0
MOUNESH-M <mounesh96290@gmail.com>
2025-07-05 18:27:47 +0530

    Voice command and Voice alert integrated

--- app.py
+++ app.py
@@ -1,8 +1,54 @@
 import cv2
-import pytesseract
 from ultralytics import YOLO
+import pytesseract
+import speech_recognition as sr
+import pyttsx3
+import threading
+import difflib
+
 
 pytesseract.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+enginee=pyttsx3.init()
+enginee.setProperty('rate', 125)
+
+camera_active=True
+
+def speak(text):
+    enginee.say(text)
+    enginee.runAndWait()
+
+def listen_to_commands():
+    global camera_active
+    recognizer=sr.Recognizer()
+    while True:
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source, duration=2)
+            print("Listing to the voice...")
+            audio=recognizer.listen(source, timeout=7, phrase_time_limit=5)
+            try:
+                command=recognizer.recognize_google(audio).lower()
+                if difflib.get_close_matches(command, ['start', 'stop', 'alert'], cutoff=0.7):
+
+                    if 'start' in command:
+                        camera_active=True
+                        speak("camera activated")
+                    elif 'stop' in command:
+                        camera_active=False
+                        speak("Camera Stopped")
+                        break
+                    elif 'alert' in command:
+                        speak("Security system in activation")
+            except sr.WaitTimeoutError:
+                print("Wait time over")
+                continue
+            except sr.UnknownValueError:
+                print("Cannot understand audio")
+                speak("unrecognitsed command")
+                continue
+            except sr.RequestError:
+                print("speech regcognisation error")
+                continue
 
 def start_detection():
     cap = cv2.VideoCapture(0)
@@ -40,13 +86,14 @@ def start_detection():
                         frame,"ALERT: Suspicious object detected..!",(50,50),
                         cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2
                         )
+                    speak(f"Alert suspicious {label} detected")
                     
         gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         text=pytesseract.image_to_string(gray)
        
 
         if text.strip()!="":
-            cv2.putText(frame, f'Text Detected: {text.strip()[:30]}', (10,450), cv2.FONT_HERSHEY_COMPLEX,7.5,(255,0,0),1)
+            cv2.putText(frame, f'Text Detected: {text.strip()[:30]}', (10,450), cv2.FONT_HERSHEY_COMPLEX,0.7,(255,0,0),2)
        
         cv2.imshow('Multi-Modal Surveillance', frame)
 
@@ -58,4 +105,5 @@ def start_detection():
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
+    threading.Thread(target=listen_to_commands).start()
     start_detection()
