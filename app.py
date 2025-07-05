cda37b6c3a555ab8811ad5a340040b712ceba6f3
MOUNESH-M <mounesh96290@gmail.com>
2025-07-05 19:18:28 +0530

    voice command droped and commands added

--- app.py
+++ app.py
@@ -1,109 +1,74 @@
 import cv2
 from ultralytics import YOLO
 import pytesseract
-import speech_recognition as sr
 import pyttsx3
-import threading
-import difflib
 
 
-pytesseract.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-enginee=pyttsx3.init()
+#tesseract path installed in the local system
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+#Speech enginee intialization
+enginee = pyttsx3.init()
+#Reducing the speed fo the voice alert
 enginee.setProperty('rate', 125)
 
-camera_active=True
-
+#function to make the voice alert
 def speak(text):
-    enginee.say(text)
-    enginee.runAndWait()
-
-def listen_to_commands():
-    global camera_active
-    recognizer=sr.Recognizer()
-    while True:
-        with sr.Microphone() as source:
-            recognizer.adjust_for_ambient_noise(source, duration=2)
-            print("Listing to the voice...")
-            audio=recognizer.listen(source, timeout=7, phrase_time_limit=5)
-            try:
-                command=recognizer.recognize_google(audio).lower()
-                if difflib.get_close_matches(command, ['start', 'stop', 'alert'], cutoff=0.7):
-
-                    if 'start' in command:
-                        camera_active=True
-                        speak("camera activated")
-                    elif 'stop' in command:
-                        camera_active=False
-                        speak("Camera Stopped")
-                        break
-                    elif 'alert' in command:
-                        speak("Security system in activation")
-            except sr.WaitTimeoutError:
-                print("Wait time over")
-                continue
-            except sr.UnknownValueError:
-                print("Cannot understand audio")
-                speak("unrecognitsed command")
-                continue
-            except sr.RequestError:
-                print("speech regcognisation error")
-                continue
+     enginee.say(text)
+     enginee.runAndWait()
 
 def start_detection():
-    cap = cv2.VideoCapture(0)
-    model = YOLO('yolov8n.pt') 
+    cap = cv2.VideoCapture(0) #for video caputring
+    model = YOLO('yolov8n.pt')  #pre trained nano You Only Look Once model developed by ultralytics
 
-    suspicious_objects=['backpack','knife','bottle','suitcase']
+    #listing the suspicious objects
+    suspicious_objects = ['backpack', 'knife', 'bottle', 'suitcase']
 
     if not cap.isOpened():
         print("Error: Cannot open camera")
         return
 
     while True:
-        ret, frame = cap.read()
+        ret, frame = cap.read() #the read function reuturn two attributes, ret(boolen) - whether the frame is read or not & frame - actuall frame
         if not ret:
             print("Error: Cannot receive frame")
             break
 
-       
         results = model.predict(source=frame, conf=0.5, stream=True)
+        #to draw the bounding box
+        for result in results: #results is a object that contians the list of predictions of the frame, result is the detection of single frame
+            boxes = result.boxes #result.boxes - all detected bounding boxes of that frame
+            names = result.names #result.names - dictionary mapping of class indices to the object names
+            for box in boxes: #iterating over all the detected boxes in a single frame
+                cls_id = int(box.cls[0]) #extracts the classID of the current detection from the class index tensor
+                label = names[cls_id] #get the corresponding Classname for the ClassID from the dictionary
 
-        
-        for result in results:
-            boxes = result.boxes
-            names=result.names
-            for box in boxes:
-                cls_id=int(box.cls[0])
-                label=names[cls_id]
+                x1, y1, x2, y2 = map(int, box.xyxy[0]) #box.xyxy gives the corrdinates of the detected object
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2) #To draw the rectangel frame around the detected object
+                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_COMPLEX, 0.9, (0, 255, 0), 2) #to write the detected object name on the rectangle box
 
-                x1,y1,x2,y2=map(int,box.xyxy[0])
-                cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
-                cv2.putText(frame,label,(x1,y1-10),cv2.FONT_HERSHEY_COMPLEX,0.9,(0,255,0),2)
-
+                #To identify the suspecious objects
                 if label in suspicious_objects:
                     cv2.putText(
-                        frame,"ALERT: Suspicious object detected..!",(50,50),
-                        cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2
-                        )
+                        frame, "ALERT: Suspicious object detected..!", (50, 50),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2
+                    )
                     speak(f"Alert suspicious {label} detected")
-                    
-        gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        text=pytesseract.image_to_string(gray)
-       
+        #text detection
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #converting to gray scale
+        text = pytesseract.image_to_string(gray)#extracting text from the frame
 
-        if text.strip()!="":
-            cv2.putText(frame, f'Text Detected: {text.strip()[:30]}', (10,450), cv2.FONT_HERSHEY_COMPLEX,0.7,(255,0,0),2)
-       
-        cv2.imshow('Multi-Modal Surveillance', frame)
+        if text.strip() != "":
+            cv2.putText(frame, f'Text Detected: {text.strip()[:30]}', (10, 450), cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 0, 0), 2)
 
-       
-        if cv2.waitKey(1) == ord('q'):
+        cv2.imshow('Multi-Modal Surveillance', frame)#to display the video
+
+        if cv2.waitKey(1) == ord('q'): #to stop the video capturing
             break
 
-    cap.release()
-    cv2.destroyAllWindows()
+    cap.release()#releasing the resource allocations
+    cv2.destroyAllWindows()#Clossing all the windows
 
 if __name__ == "__main__":
-    threading.Thread(target=listen_to_commands).start()
     start_detection()
